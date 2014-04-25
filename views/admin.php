6d372958d1abc4d26ddf9f271539d3a420af231c
J. Aaron Eaton <aaron@channeleaton.com>
2014-04-25 07:05:24 -0500

    Added translation functions #1

--- views/admin.php
+++ views/admin.php
@@ -23,22 +23,22 @@
 
 	<form method="post" action="settings.php?page=multisite-maintenance-mode">
 		<?php wp_nonce_field( 'siteoptions' ); ?>
-		<h3>Toggle Maintenance Mode</h3>
+		<h3><?php _e( 'Toggle Maintenance Mode', 'multisite-maintenance-mode' ); ?></h3>
 		<table class="form-table">
 			<tr valign="top">
-				<th scope="row"><label for="mmm-status">Set maintenance mode status</label></th>
+				<th scope="row"><label for="mmm-status"><?php _e( 'Set maintenance mode status', 'multisite-maintenance-mode' ); ?></label></th>
 				<?php
 				if ( ! get_site_option( 'mmm-status' ) )
 					update_site_option( 'mmm-status', 'off' );
 				$status = get_site_option( 'mmm-status' );
 				?>
 				<td>
-					<label><input name="mmm-status" type="radio" id="mmm-status-off" value="off" <?php checked( $status, 'off' ); ?> /> Off</label><br />
-					<label><input name="mmm-status" type="radio" id="mmm-status-on" value="on" <?php checked( $status, 'on' ); ?> /> On</label><br />
+					<label><input name="mmm-status" type="radio" id="mmm-status-off" value="off" <?php checked( $status, 'off' ); ?> /> <?php _e( 'Off', 'multisite-maintenance-mode' ); ?></label><br />
+					<label><input name="mmm-status" type="radio" id="mmm-status-on" value="on" <?php checked( $status, 'on' ); ?> /> <?php _e( 'On', 'multisite-maintenance-mode' ); ?></label><br />
 				</td>
 			</tr>
 			<tr>
-				<th scope="row"><label for="mmm-link">URL to your announcement page</label></th>
+				<th scope="row"><label for="mmm-link"><?php _e( 'URL to your announcement page', 'multisite-maintenance-mode' ); ?></label></th>
 				<?php
 				$link = get_site_option( 'mmm-link' );
 
